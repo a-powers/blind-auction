5093bbaf1220dee68ba3bda64b0c2a47212f7d93
a-powers <awpowers@gmail.com>
2021-01-31 22:36:32 +0000

    update

--- main.py
+++ main.py
@@ -3,21 +3,30 @@ from replit import clear
 from art import logo
 
 print(logo)
-name = input("What is your name?\n").lower()
-bid = int(input("What is your bid?\n$ "))
-more_bidders = input("Are there more bidders? Type 'Y' or 'N'.\n").lower
 
 bid_dict = {}
+bidding_finished = False
 
-def all_bids(new_name, new_bids):
+def final_bidder(bid_record = bid_dict):
+    highest_bid = 0
+    winner = ""
+    for bids in bid_record:
+        bid_lookup = bid_record[bids]
+        if bid_lookup > highest_bid:
+            highest_bid = bid_lookup
+            winner = bids
+    print(f"The winner is {winner} with a bid amount of ${highest_bid}.")
+
+
+
+
+while not bidding_finished:
+    name = input("What is your name?\n").lower()
+    bid = int(input("What is your bid?\n$ "))
     bid_dict[name] = bid
-
-if more_bidders == "y":
-    
-
-    
-
-
-
-all_bids(name, bid)
-print(bid_dict)
+    more_bidders = input("Are there more bidders? Type 'Y' or 'N'.\n").lower
+    if more_bidders == "N":
+        bidding_finished = True
+        final_bidder(bid_dict)
+    elif more_bidders == "Y":
+        clear()
