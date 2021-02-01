421c25570d8492fa894f4ad9403c6b558806ea1f
a-powers <awpowers@gmail.com>
2021-02-01 23:18:44 +0000

    update

---
+++ main.py
@@ -0,0 +1,10 @@
+from replit import clear
+from art import logo
+
+print(logo)
+bids = {}
+
+name = input("What is your name?")
+price = int(input("What do you bid? $")
+bids[name] = price
+print(bids)
