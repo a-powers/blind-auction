7124b446c5b7b68d54c112a733a4df7acd373b4b
a-powers <awpowers@gmail.com>
2021-02-01 23:42:12 +0000

    update

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ print(logo)
 bid_dict = {}
 bidding_finished = False
 
-while not bidding_finished:
-    names = input("What is your name?")
-    prices = int(input("What do you bid? $")
-    bid_dict[names] = prices
-    should_continue = input("Are there more bidders? Type 'y' or 'n'.")
-    if should_continue == 'n'
-    bidding_finished = True
-    elif should_continue == 'y':
-        clear()
+while not bidding_finished():
+    name = input("What is your name?\n").lower()
+    bid = int(input("What is your bid?\n$ "))
+    bid_dict[name] = bid
+
+
+more_bidders = input("Are there more bidders? Type 'Y' or 'N'.\n").lower
+
+
